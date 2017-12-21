01513bdc3f881dc337d1d1d2298f46d4a6d0601b
Serhey Shmyg <serhey.shmyg@gmail.com>
2017-12-21 14:14:24 +0200

    upgrade dist

--- src/getVendorPrefix.js
+++
@@ -1,9 +0,0 @@
-export default function getVendorPrefix() {
-    const styles = window.getComputedStyle(document.documentElement, '');
-    const res = Array.prototype.slice
-                    .call(styles)
-                    .join('')
-                    .match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o']);
-
-    return res ? res[1] : res;
-}
